e625b7e35a92a10a729ec358fe02b84bf8454504
VincentS972 <vincentsechi@gmail.com>
2023-05-02 21:11:53 -0400

    added new bread

--- controllers/bread.js
+++ controllers/bread.js
@@ -9,12 +9,27 @@ router.get('/', (req, res) => {
 })
 
 // GET bread a specific bread
+
+router.get('/new', (req, res) => {
+    res.render('new')
+})
 router.get('/:index', (req, res) => {
     const { index } = req.params
     res.render('show', {
         bread: Bread[index]
-        })
-    // res.send(Bread[index])
+    })
+})
+
+router.post('/', (req, res) => {
+    if (!req.body.image) req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+
+    Bread.push(req.body)
+    res.redirect('/breads')
 })
 
 module.exports = router
