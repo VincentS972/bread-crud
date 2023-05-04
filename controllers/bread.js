a5577b42bca0aa719f5baff624dd21c5280e240a
VincentS972 <vincentsechi@gmail.com>
2023-05-04 18:48:27 -0400

    added delete button

--- controllers/bread.js
+++ controllers/bread.js
@@ -16,7 +16,8 @@ router.get('/new', (req, res) => {
 router.get('/:index', (req, res) => {
     const { index } = req.params
     res.render('show', {
-        bread: Bread[index]
+        bread: Bread[index],
+        index: index
     })
 })
 
@@ -30,6 +31,11 @@ router.post('/', (req, res) => {
 
     Bread.push(req.body)
     res.redirect('/breads')
+})
+router.delete('/:index', (req, res) => {
+    const { index } = req.params
+    Bread.splice(index,1)
+    res.status(303).redirect('/breads')
 })
 
 module.exports = router
--- views/show.jsx
+++ views/show.jsx
@@ -1,9 +1,12 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Show ({ bread }) {
+function Show ({ bread, index }) {
     return (
         <Default>
+            <form action={`/breads/${index}?_method=DELETE`} method='POST'>
+                <input type="submit" value="DELETE" />
+            </form>
             <h3>{bread.name}</h3>
             <p>
                 and it
@@ -13,6 +16,9 @@ function Show ({ bread }) {
                 have gluten
             </p>
             <img src={bread.image} alt={bread.name} />
+            <form action={`/breads/${index}?_method=DELETE`} method='POST'>
+                <input type="submit" value="DELETE" />
+            </form>
             <li>
                 <a href={'/breads'}>Go home</a>
             </li>
